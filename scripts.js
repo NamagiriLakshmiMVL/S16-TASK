1b7e7b8050ab3b62718ee80bf90ea20b43c5e981
NamagiriLakshmiMVL <namagirimvl@gmail.com>
2023-11-16 20:48:51 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+let printing = document.querySelector(".printing")
+let c = setTimeout(() => {
+    setTimeout(() => {
+        setTimeout(() => {
+            setTimeout(() => {
+                setTimeout(()=>{
+                    setTimeout(()=>{
+                        setTimeout(()=>{
+                            setTimeout(()=>{
+                                setTimeout(()=>{
+                                    setTimeout(()=>{
+                                        setTimeout(()=>{
+                                            printing.innerText="Happy Independence day"
+                                        },1000)
+                                        printing.innerText=1
+                                    },1000)
+                                    printing.innerText=2
+                                },1000)
+                                printing.innerText=3
+                            },1000)
+                            printing.innerText=4
+                        },1000)
+                        printing.innerText=5
+                    },1000)
+                    printing.innerText=6
+                },1000)
+                printing.innerText=7
+            },1000)
+            printing.innerText=8
+        },1000)
+        printing.innerText=9
+    }, 1000)
+    printing.innerText=10
+}, 1000)
